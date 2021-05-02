1ab3c7573e3eadb6f4c3b4dbc217d13a157a2f51
Robert Nguyen <robert@gotitapp.co>
2021-05-02 19:07:19 +0700

    feat: solution + 2 extra credits

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,69 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import '@reach/dialog/styles.css'
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function LoginForm({buttonText, onSubmit}) {
+  const handleSubmit = e => {
+    e.preventDefault()
+    const {username, password} = e.target.elements
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    })
+  }
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Username:</label>
+      <input id="username" type="text" name="name" />
+      <label>Password:</label>
+      <input id="password" type="password" name="password" />
+      <button type="submit">{buttonText}</button>
+    </form>
+  )
+}
+
+function App() {
+  const [showLoginDialog, setShowLoginDialog] = useState(false)
+  const [showRegisterDialog, setShowRegisterDialog] = useState(false)
+
+  function login(formData) {
+    console.log('login', formData)
+  }
+
+  function register(formData) {
+    console.log('register', formData)
+  }
+
+  return (
+    <div>
+      <div>Bookshelf</div>
+      <Logo />
+      <div>
+        <button onClick={() => setShowLoginDialog(true)}>Login</button>
+        <Dialog isOpen={showLoginDialog}>
+          <LoginForm onSubmit={login} buttonText="Login" />
+          <div>
+            <button onClick={() => setShowLoginDialog(false)}>Dismiss</button>
+          </div>
+        </Dialog>
+      </div>
+      <div>
+        <button onClick={() => setShowRegisterDialog(true)}>Register</button>
+        <Dialog isOpen={showRegisterDialog}>
+          <LoginForm onSubmit={register} buttonText="Register" />
+          <div>
+            <button onClick={() => setShowRegisterDialog(false)}>
+              Dismiss
+            </button>
+          </div>
+        </Dialog>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
