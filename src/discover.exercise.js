610a6e0b6b5c27c80c5707136f3b33dfd432fd88
Robert Nguyen <robert@gotitapp.co>
2021-05-05 00:47:28 +0700

    feat: ex 4

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -2,35 +2,31 @@
 import {jsx} from '@emotion/core'
 
 import './bootstrap'
+import {useEffect, useState} from 'react'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+import * as colors from 'styles/colors'
+import {useAsync} from 'utils/hooks'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
+  const [query, setQuery] = useState('')
+  const [queried, setQueried] = useState(false)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
-
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  useEffect(() => { 
+    if (queried) {
+      run(client(`books?query=${encodeURIComponent(query)}`))
+    }
+  }, [queried, query, run])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    !queried && setQueried(true)
+    const {search} = event.target.elements
+    setQuery(search.value)
   }
 
   return (
@@ -59,7 +55,14 @@ function DiscoverBooksScreen() {
           </label>
         </Tooltip>
       </form>
-
+      {
+        isError ? (
+          <div css={{color: colors.danger}}>
+            <p>There was an error:</p>
+            <pre>{error.message}</pre>
+          </div>
+        ) : null
+      }
       {isSuccess ? (
         data?.books?.length ? (
           <BookListUL css={{marginTop: 20}}>
